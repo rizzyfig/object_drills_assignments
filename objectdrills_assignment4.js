3834a1bc9488d1b773c07542175d6b398349d912
Raissa <rssfiguer@gmail.com>
2017-03-10 20:46:56 -0800

    assignment 4 try

---
+++ objectdrills_assignment4.js
@@ -0,0 +1,52 @@
+// Assignment #4:
+// Write a function nameAnalyzer(name) returns an object:
+
+// {
+//   letterCount: 6,
+//   uniqueCount: 4,
+//   uniqueLetters: ['a', 'i', 'r', 's']
+// }
+
+// // change everything to lowerCase
+
+// */
+
+function nameAnalyzer(name){
+  
+
+  var myName = name.toLowerCase();
+  
+  var letterCount = myName.length
+  var uniqueCount = ""
+  var uniqueLetters = [];
+  
+//   for(var i=0; i < name.length; i++){
+
+//         for(var j=i+1; j<str_length; j++){
+
+//             if(string[i]==string[j]){
+
+//                 letterCount++
+
+//                 unique+=string[i];
+
+//             }
+
+//         }
+
+//     }
+
+ 
+
+  
+//  return var myObject = { 
+  // "letter Count:" + letterCount, 
+  // + "uniqueCount:" + uniqueCount,
+  // + "uniqueLetters" + uniqueLetters
+}
+
+
+
+
+
+console.log(nameAnalyzer("Raissa"));
